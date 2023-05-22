2e637858cc06cb00fedbd82095157256b53ae4cb
David Moina <davemolina777@gmail.com>
2023-05-22 10:12:08 -0500

    Import redux elements & future rootReducer

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+//Redux Imports
+import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
+import rootReducer from './reducer'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,4 +15,3 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
